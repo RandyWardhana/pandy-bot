6ddc44efa1d713fc9c332096845e953865549620
RandyWardhana <randywardhana37@gmail.com>
2020-06-02 06:01:39 +0700

    Create Random Message for Mabar Command

--- src/commands/mabar.js
+++ src/commands/mabar.js
@@ -1,13 +1,23 @@
 import { send } from '../response'
 import _ from 'lodash'
 
+const randomMabarMessage = (msg, args) => {
+  let param = _.isEmpty(args) ? '' : args.join(' ').toUpperCase() + ' '
+
+  const message = [
+    `> WOY MABAR ${param}@everyone!, UDAH BUKAN WAKTUNYA KERJA ATAUPUN KULIAH!1!1 TINGGALKAN SEMUA AKTIVITAS, KARENA MABAR ${args.join(' ').toUpperCase()} LEBIH PENTING... YAHAHAAAA HAAYYYUUUKKKKK`,
+    `> SENDIRI KITA JOMBLO, BERDUA KITA PACARAN, BERTIGA KITA SELINGKUH, BEREMPAT KITA MABAR ${param} @everyone! YAHAHAAAA HAAYYYUUUKKKKK`
+  ]
+
+  const randomMessage = Math.floor(Math.random() * message.length)
+  send(msg, message[randomMessage])
+}
+
 export default {
   label: 'p:mabar',
   name: 'mabar',
   value: '> Announce @everyone to play game together!',
   execute(msg, args) {
-    let param = _.isEmpty(args) ? '' : args.join(' ').toUpperCase() + ' '
-
-    send(msg, `> WOY MABAR ${param}@everyone!, UDAH BUKAN WAKTUNYA KERJA ATAUPUN KULIAH!1!1 TINGGALKAN SEMUA AKTIVITAS, KARENA MABAR ${args.join(' ').toUpperCase()} LEBIH PENTING... YAHAHAAAA HAAYYYUUUKKKKK`)
+    randomMabarMessage(msg, args)
   }
 }
